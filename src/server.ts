ff599771eaa513c24e69aa1878f3f0afa3b03713
Antoni <antoni.protoni@gmail.com>
2022-09-12 23:38:47 -0300

    subiendo a github

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const app = express();
+app.get('/ads', (request, response) => {
+  return response.json([
+    { "id": 1, name: "Anuncio primero" },
+    { "id": 2, name: "Anuncio segundo" },
+    { "id": 3, name: "Anuncio tercero" },
+    { "id": 4, name: "Anuncio cuarto" },
+  ])
+})
+
+app.listen(3333)
